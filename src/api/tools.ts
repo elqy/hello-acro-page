7a9eef2ec8773f0164fea71460e78d340037ca90
elqy <liuqinyi@163.com>
2023-02-16 23:12:06 +0800

    :package: build: 改写tools的api请求

--- src/api/tools.ts
+++ src/api/tools.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import qs from 'query-string';
+import { successResponseWrap } from '@/utils/setup-mock';
 
 export interface ToolsRecord {
   id: number;
@@ -19,10 +20,290 @@ export interface ToolsListRes {
 }
 
 export function queryToolsList(params: ToolsParams) {
-  return axios.get<ToolsListRes>('/api/list/tools', {
-    params,
-    paramsSerializer: (obj) => {
-      return qs.stringify(obj);
+  // 暂时注释掉mock方式
+  // return axios.get<ToolsListRes>('/api/list/tools', {
+  //   params,
+  //   paramsSerializer: (obj) => {
+  //     return qs.stringify(obj);
+  //   },
+  // });
+  const toolsDataList = [
+    {
+      id: 1,
+      href: 'https://htmlcheatsheet.com/js',
+      target: '_blank',
+      title: 'HTML、CSS和JavaScript速查表',
+      type: 'Web',
     },
+    {
+      id: 2,
+      href: 'https://quickref.me/javascript',
+      target: '_blank',
+      title: 'JavaScript速查表',
+      type: 'Web',
+    },
+    {
+      id: 3,
+      href: 'https://devhints.io/react/',
+      target: '_blank',
+      title: 'React.js速查表',
+      type: 'Web',
+    },
+    {
+      id: 4,
+      href: 'https://cheatography.com/',
+      target: '_blank',
+      title: 'Cheatography',
+      type: 'Web',
+    },
+    {
+      id: 5,
+      href: 'https://introcs.cs.princeton.edu/java/11cheatsheet/',
+      target: '_blank',
+      title: 'Java速查表',
+      type: 'Web',
+    },
+    {
+      id: 6,
+      href: 'https://overapi.com/',
+      target: '_blank',
+      title: 'Over API',
+      type: 'Web',
+    },
+    {
+      id: 7,
+      href: 'https://devhints.io/',
+      target: '_blank',
+      title: '开发提示',
+      type: 'Web',
+    },
+    {
+      id: 8,
+      href: 'https://gitsheet.wtf/',
+      target: '_blank',
+      title: 'gitsheet',
+      type: 'Web',
+    },
+    {
+      id: 9,
+      href: 'http://simon.html5.org/dump/html5-canvas-cheat-sheet.html',
+      target: '_blank',
+      title: 'HTML5 Canvas备忘单',
+      type: 'Web',
+    },
+    {
+      id: 10,
+      href: 'http://easings.net/en#',
+      target: '_blank',
+      title: '缓动函数',
+      type: 'Web',
+    },
+    {
+      id: 11,
+      href: 'http://moz.com/learn/seo',
+      target: '_blank',
+      title: 'SEO备忘录',
+      type: 'Web',
+    },
+    {
+      id: 12,
+      href: 'http://datav.aliyun.com/portal/graphics-engine',
+      target: '_blank',
+      title: '阿里DataV',
+      type: 'DataV',
+    },
+    {
+      id: 13,
+      href: 'https://asset.raykite.com/resource/all',
+      target: '_blank',
+      title: '光启元',
+      type: 'DataV',
+    },
+    {
+      id: 14,
+      href: 'https://store.thingjs.com/projects',
+      target: '_blank',
+      title: '优诺科技',
+      type: 'DataV',
+    },
+    {
+      id: 15,
+      href: 'http://www.hightopo.cn/demos/index.html',
+      target: '_blank',
+      title: '图扑软件',
+      type: 'DataV',
+    },
+    {
+      id: 16,
+      href: 'http://www.digihail.com/case/casesummary.html',
+      target: '_blank',
+      title: '数字冰雹',
+      type: 'DataV',
+    },
+    {
+      id: 17,
+      href: 'https://javaguide.cn/',
+      target: '_blank',
+      title: 'JavaGuide(Java面试+学习指南)',
+      type: 'Java',
+    },
+    {
+      id: 18,
+      href: 'https://www.bmrs.top/',
+      target: '_blank',
+      title: '编码人生—CodingLife',
+      type: 'Java',
+    },
+    {
+      id: 19,
+      href: 'https://icyfenix.cn/',
+      target: '_blank',
+      title: '凤凰架构-构建可靠的大型分布式系统',
+      type: 'Java',
+    },
+    {
+      id: 20,
+      href: 'https://xiaolincoding.com/',
+      target: '_blank',
+      title: '小林 x 图解计算机基础',
+      type: 'Java',
+    },
+    {
+      id: 21,
+      href: 'https://bugstack.cn/',
+      target: '_blank',
+      title: 'bugstack 虫洞栈',
+      type: 'Java',
+    },
+    {
+      id: 22,
+      href: 'https://www.wdbyte.com/',
+      target: '_blank',
+      title: '未读代码',
+      type: 'Java',
+    },
+    {
+      id: 23,
+      href: 'https://pdai.tech/',
+      target: '_blank',
+      title: 'Java 全栈知识体系',
+      type: 'Java',
+    },
+    {
+      id: 24,
+      href: 'https://tool.lu/',
+      target: '_blank',
+      title: 'Java 全栈知识体系',
+      type: 'Java',
+    },
+    {
+      id: 24,
+      href: 'https://hippo4j.cn/',
+      target: '_blank',
+      title: 'hippo4j-动态可观测线程池框架，为业务系统提高线上运行保障能力',
+      type: 'Java',
+    },
+    {
+      id: 25,
+      href: 'https://hippo4j.cn/',
+      target: '_blank',
+      title: 'hippo4j-动态可观测线程池框架，为业务系统提高线上运行保障能力',
+      type: 'Java',
+    },
+    {
+      id: 25,
+      href: 'https://arthas.aliyun.com/',
+      target: '_blank',
+      title: 'Arthas-Java 应用诊断利器',
+      type: 'Java',
+    },
+    {
+      id: 26,
+      href: 'https://www.ssssssss.org/',
+      target: '_blank',
+      title: 'magic-api接口快速开发框架',
+      type: 'Java',
+    },
+    {
+      id: 27,
+      href: 'https://gitee.com/zhengqingya/java-workspace',
+      target: '_blank',
+      title: 'Java学习笔记',
+      type: 'Java',
+    },
+    {
+      id: 27,
+      href: 'https://gitee.com/open-resource/BigData-Notes',
+      target: '_blank',
+      title: '大数据学习笔记',
+      type: 'Java',
+    },
+    {
+      id: 28,
+      href: 'https://gitee.com/log4j/pig',
+      target: '_blank',
+      title: 'pig-权限框架系统',
+      type: 'Java',
+    },
+    // freecodecamp
+    // iconpark
+    // easyAdmin
+    // GoFrame Web框架
+    // go-zero
+    // gogocode
+    // oneinstack
+    // sourcetree
+    // nginxconfig.io
+    // vant
+    // layui mini
+    // naive ui
+    // Erupt
+    // Amis
+    // 若依
+    // semi Design
+    // arcoDesign
+    // dataV
+    // jeecgBoot
+    // Hutool
+    // Chiner
+    // ApiFox
+    // QingScan
+    // DongTai
+    // Open IM
+    // Pear Admin
+    // AntV
+    // Vben Admin
+    // tauri
+    // likeAdmin
+    // Mitosis
+    // mini sandbox
+    // Simple Admin
+    // DataEase
+    // Kratos
+    // LiteFlow
+    // Sa-Token-Quick-Login
+    // Rancher
+    // 建木
+    // Wx-java
+    // jeepay
+    //
+  ];
+  // const {
+  //   current = 1,
+  //   pageSize = 10,
+  //   type = '',
+  // } = qs.parseUrl(params.url).query;
+  const { current } = params;
+  const { pageSize } = params;
+  const { type } = params;
+  const p = current as number;
+  const ps = pageSize as number;
+  const t = type as string;
+  const result = toolsDataList
+    .filter((item) => item.type === t)
+    .slice((p - 1) * ps, p * ps);
+  return successResponseWrap({
+    list: result,
+    total: result.length,
   });
 }
