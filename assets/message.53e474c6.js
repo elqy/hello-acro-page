9b942fab40dc3b56ef5567de2bf19ddf37036e61
elqy <liuqinyi@163.com>
2023-02-16 14:42:31 +0000

    Deploying to gh-pages from @ elqy/hello-arco-page@bea4847c7aa33710e8e3703d19e4d3b1110a0b79 🚀

--- assets/message.53e474c6.js
+++
@@ -1,1 +0,0 @@
-import{h as s}from"./index.0880459b.js";function e(){return s.post("/api/message/list")}function i(t){return s.post("/api/message/read",t)}function r(){return s.post("/api/chat/list")}export{r as a,e as q,i as s};
---
+++ assets/message.e5b3c988.js
@@ -0,0 +1,1 @@
+import{h as s}from"./index.2bc82280.js";function e(){return s.post("/api/message/list")}function i(t){return s.post("/api/message/read",t)}function r(){return s.post("/api/chat/list")}export{r as a,e as q,i as s};
