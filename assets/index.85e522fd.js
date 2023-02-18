845be1ad8ab73c194867c4e4042c93cafeba9124
elqy <liuqinyi@163.com>
2023-02-18 11:24:10 +0000

    Deploying to gh-pages from @ elqy/hello-arco-page@8eea80044227f1f991de3351bb8e23c1ef40b146 🚀

---
+++ assets/index.85e522fd.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./index.aa45b352.js";import{A as t,aC as _,aF as a,aL as c,bc as r}from"./arco.603f0d7f.js";const s={};function n(f,d){const e=r;return t(),_(e,{class:"footer"},{default:a(()=>[c("Arco Pro")]),_:1})}const x=o(s,[["render",n],["__scopeId","data-v-ef0e5303"]]);export{x as F};
