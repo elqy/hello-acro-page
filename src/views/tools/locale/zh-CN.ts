f7e01f1e2e1cbf7d4db83534e290d3ebb743b89f
elqy <liuqinyi@163.com>
2023-02-18 22:06:04 +0800

    :sparkles: feat: 新增菜单数据

--- src/views/tools/locale/zh-CN.ts
+++ src/views/tools/locale/zh-CN.ts
@@ -4,6 +4,8 @@ export default {
   'tool.tab.title.web': '前端',
   'tool.tab.title.java': 'Java',
   'tool.tab.title.python': 'Python',
+  'tool.tab.title.go': 'Go',
   'tool.tab.title.datav': '数据可视化',
+  'tool.tab.title.other': '其他',
   'tool.searchInput.placeholder': '搜索',
 };
